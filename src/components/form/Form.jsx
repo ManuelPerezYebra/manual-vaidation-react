25b4cb93c5e2956ce7b737110335071aec216197
Manuel Perez Yebra <154430764+ManuelPerezYebra@users.noreply.github.com>
2024-06-19 17:30:47 +0200

    finish

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,15 +1,136 @@
-import { StyledForm } from "./form.styles"
+import { useState } from "react";
+import {  ErrorText, StyledForm, StyledInput } from "./form.styles";
 
-const Form = ()=>{
-    return <StyledForm>
-        <input type="text" name="name" id="name" placeholder="Name" />
-        <input type="text" name="lastname" id="lastname" placeholder="Last Name"/>
-        <input type="email" name="mail" id="mail" placeholder="Email Address"/>
-        <input type="password" name="password" id="password" placeholder="Password"/>
-        <input type="submit" value="CLAIM YOUR FREE TRIAL" />
-        <p>By clicking the button, you are agreeing to our <span>Terms and Services</span>
-             </p>
-    </StyledForm>
-}
+const validateForm = (name, value, setFormData) => {
+    const formatedValue = value.trim();
+    const regexName = /^[a-z]+$/i;
+    const regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+    
+    let isValid = true;
 
-export default Form
+    if (name === 'name') {
+        isValid = regexName.test(formatedValue) || formatedValue === "";
+        setFormData((prevState) => ({
+            ...prevState,
+            errors: {
+                ...prevState.errors,
+                name: formatedValue === "" ? false : !isValid
+            }
+        }));
+    }else if (name ==='lastname'){
+        isValid = regexName.test(formatedValue)|| formatedValue==='';
+        setFormData((prevState)=>({
+            ...prevState,
+            errors: {
+                ...prevState.errors,
+                lastname: formatedValue===''? false : !isValid
+            }
+        }))
+    }else if (name==='mail'){
+        isValid=regexMail.test(formatedValue) || formatedValue ==='';
+        setFormData((prevState)=>({
+            ...prevState,
+            errors:{
+                ...prevState.errors,
+                mail: formatedValue==='' ? false : !isValid
+            }
+        }))
+    }else if(name==='password'){
+        isValid=regexPassword.test(formatedValue) || formatedValue === '';
+        setFormData((prevState)=>({
+            ...prevState,
+            errors:{
+                ...prevState.errors,
+                password: formatedValue === '' ? false : !isValid
+            }
+        }))
+    }
+    // Añadir validación para otros campos según sea necesario
+};
+
+const changeDataValues = (event, setFormData) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => ({
+        ...prevState,
+        [name]: value
+    }));
+    validateForm(name, value, setFormData);
+};
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        lastname: '',
+        mail: '',
+        password: '',
+        errors: {}
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Lógica de envío del formulario
+        console.log('Form submitted:', formData);
+    };
+
+    return (
+        <StyledForm onSubmit={handleSubmit}>
+            <label htmlFor="name" >
+                <img src="icon-error.svg" alt=""  style={{ visibility: formData.errors.name ? 'visible' : 'hidden' }}/>
+                <StyledInput 
+                    type="text"
+                    name="name"
+                    id="name"
+                    $outline={formData.errors.name}
+                    $hasError={formData.errors.name}
+                    placeholder="Name"
+                    onInput={(event) => changeDataValues(event, setFormData)}
+                />
+                <ErrorText style={{ visibility: formData.errors.name ? 'visible' : 'hidden' }}>El nombre no es Válido</ErrorText>
+            </label>
+            <label htmlFor="lastname">
+                <img src="icon-error.svg" alt="" style={{ visibility: formData.errors.lastname ? 'visible' : 'hidden' }} />
+                <StyledInput
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    $outline={formData.errors.lastname}
+                    $hasError={formData.errors.lastname}
+                    placeholder="Last Name"
+                    onInput={(event) => changeDataValues(event, setFormData)}
+                />
+                <ErrorText style={{ visibility: formData.errors.lastname ? 'visible' : 'hidden' }}>El Apellido no es Válido</ErrorText>
+            </label>
+            <label htmlFor="mail">
+                <img src="icon-error.svg" alt="" style={{ visibility: formData.errors.mail ? 'visible' : 'hidden' }} />
+                <StyledInput
+                    type="email"
+                    name="mail"
+                    id="mail"
+                    $outline={formData.errors.mail}
+                    $hasError={formData.errors.mail}
+                    placeholder="Email Address"
+                    onInput={(event) => changeDataValues(event, setFormData)}
+                />
+                <ErrorText style={{ visibility: formData.errors.mail ? 'visible' : 'hidden' }}>El mail no es Válido</ErrorText>
+            </label>
+            <label htmlFor="password">
+                <img src="icon-error.svg" alt="" style={{ visibility: formData.errors.password ? 'visible' : 'hidden' }} />
+                <StyledInput
+                    type="password"
+                    name="password"
+                    id="password"
+                    $outline={formData.errors.password}
+                    $hasError={formData.errors.password}
+                    placeholder="Password"
+                    onInput={(event) => changeDataValues(event, setFormData)}
+                />
+                <ErrorText style={{ visibility: formData.errors.password ? 'visible' : 'hidden' }}>La contraseña debe de tener letras mayúsculas, minúsculas y un carácter especial</ErrorText>
+            </label>
+            <input type="submit" value="CLAIM YOUR FREE TRIAL" />
+            <p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
+        </StyledForm>
+    );
+};
+
+export default Form;
--- src/components/form/form.styles.js
+++ src/components/form/form.styles.js
@@ -1,32 +1,44 @@
 import styled from 'styled-components';
+const StyledInput = styled.input`
+	width: 100%;
+	margin: 0 auto;
+	height: 56px;
+	border-radius: 10px;
+	border: ${({ $hasError }) =>
+		$hasError ? '1px solid red' : '1px solid #DEDEDE'};
+	padding-left: 15px;
+	font-weight: 700;
+	position: relative;
+	&:focus {
+		outline-color: ${({ $outline }) => ($outline ? 'red' : '#DEDEDE')};
+	}
+`;
 
 const StyledForm = styled.div`
 	width: 90%;
-	height: 474px;
+	height: 500px;
 	background-color: white;
 	margin: 0 auto;
 	border-radius: 10px;
 	box-shadow: 0px 10px 5px 0px rgba(0, 0, 0, 0.28);
 	display: flex;
 	flex-direction: column;
-	gap: 20px;
+	gap: 25px;
 	padding-top: 40px;
 	font-family: 'Poppins';
-	input {
-		width: 80%;
-		margin: 0 auto;
+	input[type='submit'] {
+		border: none;
 		height: 56px;
 		border-radius: 10px;
-		border: 1px solid #dedede;
-		padding-left: 15px;
-		font-weight: 700;
-	}
-	input[type='submit'] {
+		cursor: pointer;
 		background-color: #38cc8b;
 		color: white;
 		box-shadow: 0px -11px 4px -4px rgba(51, 185, 126, 0.8) inset;
 		font-size: 15px;
 		font-weight: 600;
+		width: 80%;
+		margin: 0 auto;
+		margin-top: 20px;
 	}
 	p {
 		font-size: 11px;
@@ -37,6 +49,21 @@ const StyledForm = styled.div`
 		color: #ff7979;
 		font-weight: bold;
 	}
+	label {
+		width: 80%;
+		margin: 0 auto;
+		position: relative;
+
+		border-radius: 10px;
+		img {
+			position: absolute;
+			right: 20px;
+			z-index: 1;
+			top: 50%;
+			transform: translateY(-50%);
+			visibility: ${props => (props.visible ? 'visible' : 'hidden')};
+		}
+	}
 	@media (max-width: 480px) {
 		p {
 			padding-left: 39px;
@@ -45,5 +72,10 @@ const StyledForm = styled.div`
 		}
 	}
 `;
-
-export { StyledForm };
+const ErrorText = styled.span`
+	position: absolute;
+	left: 10px;
+	top: 60px;
+	font-size: 12px;
+`;
+export { StyledForm, ErrorText, StyledInput };
